b48741684c8719d294197ccfe8d1092a66668633
nataliathomazelliferreira <nataliathomazelliferreira@gmail.com>
2026-03-24 09:10:50 -0300

    editando dados

--- controle_produto.py
+++ controle_produto.py
@@ -1,18 +1,18 @@
-class Produto: #criar um dado novo, chama produto, esse dado vai ter nome, preco de compra, preco de venda, data de compra, data de validade e quantidade.
-    #def vou usar para definir uma funcao
-    #def init estou criando uma funcao dentro dessa classe, quando alguem criar um produto vai executar isso automaticamente
-    #def init 
+from datetime import datetime #estou mexendo com datas
+
+class Produto:
+
     def __init__(self, nome, preco_compra, preco_venda, data_compra, data_validade, quantidade):
         self.nome = nome
         self.preco_compra = preco_compra
         self.preco_venda = preco_venda
-        self.data_compra = data_compra
-        self.data_validade = data_validade
+        self.data_compra = datetime.strptime(data_compra, "%d/%m/%Y")
+        self.data_validade = datetime.strptime(data_validade, "%d/%m/%Y")
         self.quantidade = quantidade
-#def str para definir como o objeto aparece quando eu uso print
-    def __str__(self):
-        return f"{self.nome} | Preço: R${self.preco_venda} | Quantidade: {self.quantidade} | Validade: {self.data_validade}"
 
-# classe Produto: serve como um molde para criar produtos da cantina, e todo produto terá as mesmas características, que são os atributos.
-# objeto: cada produto criado (ex: coca, água) é um objeto da classe Produto
-# atributos: sao as características de cada produto: nome, preco, quantidade, validade
+    def esta_vencido(self):
+        return datetime.now() > self.data_validade
+
+    def __str__(self):
+        status = "VENCIDO" if self.esta_vencido() else "OK"
+        return f"{self.nome} | Preço: R${self.preco_venda} | Qtde: {self.quantidade} | Validade: {self.data_validade.strftime('%d/%m/%Y')} | {status}"
