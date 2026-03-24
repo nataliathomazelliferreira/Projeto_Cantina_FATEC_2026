7480c5176a166043edeeb8628ce66dccfc42a35f
nataliathomazelliferreira <nataliathomazelliferreira@gmail.com>
2026-03-24 10:13:12 -0300

    editando estoque

--- controle_estoque.py
+++ controle_estoque.py
@@ -1,33 +1,27 @@
-from produto import Produto
+from controle_produto import Produto
 
-class Estoque: 
-    #def = definir uma função
-    #__init__ = função especial que roda quando o objeto é criado
-    #self = o próprio objeto
-    #quando eu criar um estoque, isso aqui vai acontecer automaticamente
+class Estoque:
 
     def __init__(self):
         self._produtos = []
-#self = esse estoque
-# _produtos = nome da lista, _ no produto significa que esta encapsulado
-#[] = lista vazia
-        
+
     def adicionar_produto(self, produto):
         self._produtos.append(produto)
-#adicionar os produtos
-#append para colocar os produtos dentro da lista
 
     def mostrar_produtos(self):
         for produto in self._produtos:
             print(produto)
-    #mostrar tudo que tenho de produtos
-    #
 
     def vender_produto(self, nome_produto):
-#vender produto pelo nome dele
 
         for produto in self._produtos:
+
             if produto.nome == nome_produto:
+
+                if produto.esta_vencido():
+                    print("Produto vencido! Não pode vender.")
+                    return None
+
                 if produto.quantidade > 0:
                     produto.quantidade -= 1
                     print(f"Venda realizada: {produto.nome}")
